ebf057e4602530810af41f93f84e248962656550
macbook <macbook@macbooks-MacBook-Pro.local>
2020-08-06 18:31:24 -0400

    Updating files

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -3,16 +3,30 @@
 
   let lightBox = document.querySelector(".lightbox"),
       lbClose = lightBox.querySelector("span"),
+      lbVideo = lightBox.querySelector("video"),
       sigils = document.querySelectorAll(".sigilContainer");
 
-  function showLightbox() {
-    lightBox.classList.add('show-lightbox');
+  function showHideLightbox() {
+    lightBox.classList.toggle('show-lightbox');
+
+    // try making the video play as well
+
+    // check the play state of the video.
+    // if it's paused or not playing, that means we just opened the lightBox
+    // and we want the video to play
+    if (lbVideo.paused) {
+      lbVideo.play();
+    } else {
+      // lightbox is open and we want the video to rewind and stop when we close it.
+      lbVideo.currentTime = 0;
+      lbVideo.pause();
+    }
+    // rewind the video and make it pause at currentTime of 0
   }
 
-  function hideLightbox() {
-    lightBox.classList.remove('show-lightbox');
-  }
+  sigils.forEach(sigil => sigil.addEventListener("click", showHideLightbox));
+  lbClose.addEventListener("click", showHideLightbox);
 
-  sigils.forEach(sigil => sigil.addEventListener("click", showLightbox));
-  lbClose.addEventListener("click", hideLightbox);
+  // add an "ended" event handler for the video -> close the lightBox
+  lbVideo.addEventListener("ended", showHideLightbox);
 })();
